4b65ca6ebd7b803de862226c0ada02b390e8699a
dcasco02 <dcasco@unitec.edu>
2022-04-22 13:41:56 -0600

    matrices

--- src/diegocasco_lab1p2/DiegoCasco_Lab1P2.java
+++ src/diegocasco_lab1p2/DiegoCasco_Lab1P2.java
@@ -15,7 +15,39 @@ public class DiegoCasco_Lab1P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     int size=8;
+     char [][]matriz_rojo=new char[size][size];
+     char [][]matriz_azul=new char[size][size];
+     matriz_rojo = llenado(size);
+     int barcos_rojo=3;
+     int barcos_azul=3;
+     
     }
-    
+    public static char[][]llenado(int size){
+        char [][] temporal = new char [size][size];
+        for(int i=0;i<size;i++){
+            for(int j=0; j<size;j++){
+                temporal[i][j]=0;
+            }
+        }
+        return temporal;
+    }
+    public static void Imprimirrojo(char [][]matriz_rojo){
+        //int []columnas=new int[numeros[0].length];
+         for(int i=0;i<matriz_rojo.length;i++){
+            for(int j=0; j<matriz_rojo[i].length;j++){
+                System.out.print("{"+matriz_rojo[i][j]+ "} ");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void Imprimirazul(char [][]matriz_azul){
+        //int []columnas=new int[numeros[0].length];
+         for(int i=0;i<matriz_azul.length;i++){
+            for(int j=0; j<matriz_azul[i].length;j++){
+                System.out.print("{"+matriz_azul[i][j]+ "} ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
