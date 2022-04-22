83ea3dafe054e709910b8c24613337f9afadf6b2
dcasco02 <dcasco@unitec.edu>
2022-04-22 14:35:50 -0600

    avance 3

--- src/diegocasco_lab1p2/DiegoCasco_Lab1P2.java
+++ src/diegocasco_lab1p2/DiegoCasco_Lab1P2.java
@@ -12,50 +12,57 @@ import java.util.Scanner;
  * @author dcasc
  */
 public class DiegoCasco_Lab1P2 {
-
+static Scanner lea=new Scanner(System.in); 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     Scanner lea=new Scanner(System.in); 
-     int size=8;
-     char [][]matriz_rojo=new char[size][size];
-     char [][]matriz_azul=new char[size][size];
-     matriz_rojo = llenado(size);
-     matriz_azul = llenado(size);
-     int barcos_rojo=3;
-     int barcos_azul=3;
-     for(int i=1;i==barcos_rojo;i++){
-        String coordenadas;
-        System.out.println("Ingrese las coordenadas de Barcos rojos");
-        coordenadas=lea.next();
-        String []tokens=coordenadas.split(",");
-        []tokens.CharAt(1)
-     }
+        int size = 8;
+        char[][] matriz_rojo = new char[size][size];
+        char[][] matriz_azul = new char[size][size];
+        matriz_rojo = llenado(size);
+        matriz_azul = llenado(size);
+        int barcos_rojo = 0;
+        int barcosazul = 0;
+        while(barcos_rojo>=3)
+        Imprimirazul(matriz_azul);
+        Imprimirrojo(matriz_rojo);
+        for (int i = 1; i == 3; i++) {
+            String coordenadas;
+            char Coordenadas;
+            System.out.println("Ingrese las coordenadas de Barcos rojos");
+            coordenadas = lea.next();
+            String[] tokens = coordenadas.split(",");
+            Coordenadas = tokens[0].charAt(1);
+            System.out.println(Coordenadas);
+        }
     }
-    public static char[][]llenado(int size){
-        char [][] temporal = new char [size][size];
-        for(int i=0;i<size;i++){
-            for(int j=0; j<size;j++){
-                temporal[i][j]=0;
+
+    public static char[][] llenado(int size) {
+        char[][] temporal = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                temporal[i][j] = 0;
             }
         }
         return temporal;
     }
-    public static void Imprimirrojo(char [][]matriz_rojo){
+
+    public static void Imprimirrojo(char[][] matriz_rojo) {
         //int []columnas=new int[numeros[0].length];
-         for(int i=0;i<matriz_rojo.length;i++){
-            for(int j=0; j<matriz_rojo[i].length;j++){
-                System.out.print("{"+matriz_rojo[i][j]+ "} ");
+        for (int i = 0; i < matriz_rojo.length; i++) {
+            for (int j = 0; j < matriz_rojo[i].length; j++) {
+                System.out.print("{" + matriz_rojo[i][j] + "} ");
             }
             System.out.println(" ");
         }
     }
-    public static void Imprimirazul(char [][]matriz_azul){
+
+    public static void Imprimirazul(char[][] matriz_azul) {
         //int []columnas=new int[numeros[0].length];
-         for(int i=0;i<matriz_azul.length;i++){
-            for(int j=0; j<matriz_azul[i].length;j++){
-                System.out.print("{"+matriz_azul[i][j]+ "} ");
+        for (int i = 0; i < matriz_azul.length; i++) {
+            for (int j = 0; j < matriz_azul[i].length; j++) {
+                System.out.print("{" + matriz_azul[i][j] + "} ");
             }
             System.out.println(" ");
         }
